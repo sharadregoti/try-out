5fcf97bc98a9b86b82bbec1e74341b72bd7a2b57
xi3161-sregoti <sharad.regoti@xebia.com>
2022-12-04 12:55:33 +0530

    m

--- 03-open-telemetry/monitoring/prometheus-instrumentation/main.go
+++ 03-open-telemetry/monitoring/prometheus-instrumentation/main.go
@@ -16,9 +16,8 @@ func add(w http.ResponseWriter, req *http.Request) {
 
 	fmt.Println(req.URL.Query())
 
-	num1, err1 := strconv.Atoi(req.URL.Query().Get("num1"))
-	num2, err2 := strconv.Atoi(req.URL.Query().Get("num2"))
-	fmt.Println(err1, " s", err2)
+	num1, _ := strconv.Atoi(req.URL.Query().Get("num1"))
+	num2, _ := strconv.Atoi(req.URL.Query().Get("num2"))
 
 	json.NewEncoder(w).Encode(map[string]interface{}{"result": num1 + num2})
 }
